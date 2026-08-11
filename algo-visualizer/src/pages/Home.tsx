3fbff46157a385c6fae1076cec3c70eb73d1b94d
Ricardo Woo <wooosmar@gmail.com>
2026-08-11 17:08:43 -0500

    feat: update Home component to use Navbar and Hero components

--- algo-visualizer/src/pages/Home.tsx
+++ algo-visualizer/src/pages/Home.tsx
@@ -1,12 +1,11 @@
-import { Button } from "../components/Button";
-import { NavBar } from "../components/Navbar";
+import Hero from "../components/Hero";
+import { Navbar } from "../components/Navbar";
 
 export function Home() {
   return (
     <>
-      <NavBar />
-      <h1 className="text-3xl font-bold">Home Page</h1>
-      <Button display="Press" />
+      <Navbar />
+      <Hero />
     </>
   );
 }
