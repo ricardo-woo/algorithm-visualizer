d453da3db379fba7e57bd5f2513ddcd7be2701e5
Ricardo Woo <wooosmar@gmail.com>
2026-08-12 16:27:04 -0500

    feat: integrate Navbar component into App and adjust routing structure

--- algo-visualizer/src/App.tsx
+++ algo-visualizer/src/App.tsx
@@ -2,12 +2,14 @@ import "./App.css";
 import { Home } from "./pages/Home";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Playground from "./pages/Playground";
+import { Navbar } from "./components/Navbar";
 
 function App() {
   return (
     <>
-      <BrowserRouter basename="/algorithm-visualizer">
+      <BrowserRouter basename="/algorithm-visualizer/">
         <div className="bg-background dark:bg-dark-background">
+          <Navbar />
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/playground" element={<Playground />} />
--- algo-visualizer/src/pages/Home.tsx
+++ algo-visualizer/src/pages/Home.tsx
@@ -1,11 +1,9 @@
 import Card from "../components/CardGrid";
 import Hero from "../components/Hero";
-import { Navbar } from "../components/Navbar";
 
 export function Home() {
   return (
     <>
-      <Navbar />
       <div className="mx-auto w-full max-w-7xl px-4 sm:px-6 lg:px-6">
         <Hero />
         <Card />
