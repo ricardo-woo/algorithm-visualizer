13621b3fd326c40adfa2775aa664e49b7d85fc5c
Ricardo Woo <wooosmar@gmail.com>
2026-08-14 16:39:47 -0500

    fix: remove unused import from vite.config.ts

--- algo-visualizer/vite.config.ts
+++ algo-visualizer/vite.config.ts
@@ -2,9 +2,7 @@ import { defineConfig } from "vite";
 import react, { reactCompilerPreset } from "@vitejs/plugin-react";
 import babel from "@rolldown/plugin-babel";
 import tailwindcss from "@tailwindcss/vite";
-import { off } from "process";
 
-// https://vite.dev/config/
 export default defineConfig({
   base: "/algorithm-visualizer/",
   root: "..",
